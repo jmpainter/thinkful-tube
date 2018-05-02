97a7530f73e61142bb27667ae5fe97ccc3422695
jmpainter <jpainter4@gmail.com>
2018-05-02 11:02:46 -0700

    added channel functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const YOUTUBE_EMBED_URL = 'https://www.youtube.com/embed/'
+https://www.youtube.com/embed/tgbNymZ7vqY
 
-function getDataFromApi(searchTerm, callback) {
+function getSearchTermDataFromApi(searchTerm, callback) {
   const query = {
     part: 'snippet',
     key: 'AIzaSyDz1msOIOSeKQO_-4y4_RTn7r5v7fI7OPI',
@@ -9,14 +11,23 @@ function getDataFromApi(searchTerm, callback) {
   $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
 
+function getChannelDataFromApi(channelId, callback) {
+  const query = {
+    part: 'snippet',
+    key: 'AIzaSyDz1msOIOSeKQO_-4y4_RTn7r5v7fI7OPI',
+    channelId: channelId
+  };
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
 function renderResult(result) {
   return `
-  <li>
+  <li id="${result.id.videoId}">
     <a href="#">
-      <img class="thumbnail" id="${result.id.videoId}" src="${result.snippet.thumbnails.default.url}" alt="thumbnail">
+      <img class="js-thumbnail thumbnail" src="${result.snippet.thumbnails.default.url}" alt="thumbnail">
     </a>
-    <p class="title">${result.snippet.title}</p>
-    <p class="source"><a href="#" id="${result.snippet.channelId}">${result.snippet.channelTitle}</a></p>
+    <p class="title"><a class="js-title" href="#">${result.snippet.title}</a></p>
+    <p class="source"><a href="#" class="js-channel" id="${result.snippet.channelId}">${result.snippet.channelTitle}</a></p>
   </li>
   `;
 }
@@ -32,21 +43,39 @@ function handleSearchSubmit() {
     event.preventDefault();
     const query = $(event.currentTarget).find('#js-query').val();
     console.log(query);
-    getDataFromApi(query, displayResults)
+    getSearchTermDataFromApi(query, displayResults)
   });
 }
 
+function setVideo(videoId) {
+  $('.js-video').attr('src', YOUTUBE_EMBED_URL + videoId);
+}
+
 function handleThumbnailClick() {
-  $('.results').on('click', '.thumbnail', function(event) {
-    const videoId = $(this).attr('id');
-    $('.js-video').attr('src', 'https://www.youtube.com/watch?v=' + videoId);
-    // https://www.youtube.com/watch?v=xW0i-1iIFrI
+  $('.results').on('click', '.js-thumbnail', function(event) {
+    setVideo($(this).closest('li').attr('id'));
+  });
+}
+
+function handleTitleClick() {
+  $('.results').on('click', '.js-title', function(event) {
+    setVideo($(this).closest('li').attr('id'));
+  });
+}
+
+function handleChannelClick() {
+  $('.results').on('click', '.js-channel', function(event) {
+    const channelId = $(this).attr('id');
+    console.log(channelId);
+    getChannelDataFromApi(channelId, displayResults)
   });
 }
 
 function startApp() {
   handleSearchSubmit();
   handleThumbnailClick();
+  handleTitleClick();
+  handleChannelClick();
 }
 
 $(startApp);
